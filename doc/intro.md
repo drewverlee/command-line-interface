c098421dc9cec78266f9f0f8652cc70e8a80c305
drew verlee <drew.verlee@gmail.com>
2018-01-06 11:16:56 -0500

    Setup lein lib project defaults

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to command-line-interface
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/command_line_interface/core_test.clj
@@ -0,0 +1,7 @@
+(ns command-line-interface.core-test
+  (:require [clojure.test :refer :all]
+            [command-line-interface.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
